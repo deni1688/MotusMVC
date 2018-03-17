fb3a19d76f26bd957bbf523979876101be60fd1d
deni1688 <denisj1688@gmail.com>
2018-03-17 20:05:33 +0100

    updated comments

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,14 +1,14 @@
 <?php
 
 
-// default controller
+// default controller set in Controller Class
 class Pages extends Controller
 {
     public function __construct()
     {
     }
 
-    // default method
+    // default method set in Controller Class
     public function index()
     {
         $data = [
